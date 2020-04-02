8c39fea9726f4a19e5c369a7096fd1103a4f0d4a
fasnicar <francesco.asnicar@gmail.com>
2020-04-02 18:15:24 +0200

    expliciting --extract

--- preprocessing/fastqc.sh
+++ preprocessing/fastqc.sh
@@ -2,13 +2,16 @@
 
 
 # conda deactivate && conda activate fastqc
+# parallel -j10 "../fastqc.sh {}" ::: `ls`
 
 echo $1;
+
+mkdir -p $1/fastqc_raw;
 
 for i in $(ls $1/*.fastq.gz); do
     echo "    $i";
     gzip -dk $i;
-    fastqc $i -o $1/fastqc_raw/ -t 8 -f fastq -q;
+    fastqc $i -o $1/fastqc_raw/ -t 8 -f fastq --extract -q;
     rm ${i%.gz};
 done;
 
