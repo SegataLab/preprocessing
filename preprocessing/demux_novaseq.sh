8f7e7ed74387f73b907983ef99ef6de7bf28b3c6
fasnicar <francesco.asnicar@gmail.com>
2022-07-05 14:51:20 +0000

    reverse sorting to avoid samples with common prefix being moved to only one folder

--- preprocessing/demux_novaseq.sh
+++ preprocessing/demux_novaseq.sh
@@ -4,4 +4,4 @@ ulimit -n 999999;
 bcl2fastq --barcode-mismatches 1 -r 16 -w 16 -p 32 -R RUN_FOLDER --sample-sheet SAMPLE_SHEET.CSV -o OUTPUT_FOLDER
 
 #rm -r Reports/ Stats/ Undetermined_S0_L00*.fastq.gz
-#for i in $(ls *.fastq.gz | rev | cut -f5- -d'_' | rev | sort | uniq); do mkdir ${i}; echo $i; mv ${i}_*.fastq.gz ${i}/; done
+#for i in $(ls *.fastq.gz | rev | cut -f5- -d'_' | rev | sort -r | uniq); do mkdir ${i}; echo $i; mv ${i}_*.fastq.gz ${i}/; done
